48e1fb0238e35a66c07e32e2e52dc89b57e4f620
inmokang <inmo.kang@gmail.com>
2025-04-07 05:24:54 +0900

    first commit

---
+++ src/main/java/com/example/keycloak/site1_auth/Site1AuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site1_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Site1AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Site1AuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keycloak/site1_auth/Site1AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak.site1_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Site1AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
